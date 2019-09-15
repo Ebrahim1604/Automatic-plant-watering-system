ff65a4ea8f9649e4bcc9e04b6c1dcc8368bd4005
Ebrahim1604 <42499258+Ebrahim1604@users.noreply.github.com>
2019-09-15 12:26:14 +0530

    updated with custom analog read

--- moisture_sensor_test.ino
+++ moisture_sensor_test.ino
@@ -25,13 +25,6 @@ void loop() {
   delay(1000);
  }
 
-void analogReference(uint8_t mode)
-{
-  // can't actually set the register here because the default setting
-  // will connect AVCC and the AREF pin, which would cause a short if
-  // there's something connected to AREF.
-  analog_reference = mode;
-}
 
 int read_analog_pin(uint8_t pin)
 {
